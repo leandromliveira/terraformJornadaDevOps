4fd7ab3930ffddeb14526dd2acb31630b124889c
Leandro Magalhães Oliveira <lemagalhaes@uolinc.com>
2024-07-05 13:31:00 -0300

    add secrets para testes

--- var.tf
+++ var.tf
@@ -9,3 +9,55 @@ variable "do_token" {
 variable "ssh_key_name" {
   default = ""
 }
+
+variable "aws_access_key_1" {
+  default = "AKIAIOSFODNN7EXAMPLE1"
+}
+
+variable "aws_secret_key_1" {
+  default = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY1"
+}
+
+variable "aws_access_key_2" {
+  default = "AKIAIOSFODNN7EXAMPLE2"
+}
+
+variable "aws_secret_key_2" {
+  default = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY2"
+}
+
+variable "aws_access_key_3" {
+  default = "AKIAIOSFODNN7EXAMPLE3"
+}
+
+variable "aws_secret_key_3" {
+  default = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY3"
+}
+
+variable "aws_access_key_4" {
+  default = "AKIAIOSFODNN7EXAMPLE4"
+}
+
+variable "aws_secret_key_4" {
+  default = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY4"
+}
+
+variable "aws_access_key_5" {
+  default = "AKIAIOSFODNN7EXAMPLE5"
+}
+
+variable "aws_secret_key_5" {
+  default = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY5"
+}
+
+variable "slack_webhook_1" {
+  default = "https://hooks.slack.com/services/T0FAKEH1C/B017GAZERA7/qfOQ6ExaMpLeBY6i2ZJssjr1"
+}
+
+variable "slack_webhook_2" {
+  default = "https://hooks.slack.com/services/T0FAKEH1C/B017GAZERA7/qfOQ6ExaMpLeBY6i2ZJssjr2"
+}
+
+variable "grafana_api_key" {
+  default = "eyJrIjoiRlhExamPlEg4YXlkS2h3RlFtbTFFQk1HazFMSmpaVWgiLCJuIjoicGFnc2VndXJvX29wZXIiLCJFakeH0="
+}
